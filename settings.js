b560781aa2c64f6a6ee3a89db12bf8ce7ffa3811
liquidsnake12 <141171387+liquidsnake12@users.noreply.github.com>
2023-10-04 21:00:46 +0000

    settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['agent-47'] //ur owner number
+global.ownernomer = "47" //ur owner number2
+global.ownername = "VenomSnake" //ur owner name
+global.ytname = "nah" //ur yt chanel name
+global.socialm = "GitHub: nah" //ur github or insta name
+global.location = "no location" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "SnakeBot"
+global.ownernumber = '47'
+global.ownername = 'VenomSnake'
+global.ownerNumber = ["nothing here "]
+global.ownerweb = ""
+global.websitex = ""
+global.wagc = ""
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.botscript = '' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = ""
+global.creator = ""
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
